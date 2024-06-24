fd3a98548028c0742d37ca5cd2a5d4dedb3c1ddf
yx <17879765153@163.com>
2024-06-24 20:21:58 +0800

    6.24

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,27 @@
 <template>
-	<h1>你好，这是首页
-	</h1>
+
+	<view class="body">
+		<view class="swpierBox">
+			<swiper>
+				<swiper-item>
+					<image src="../../2.jpg" mode="aspectFit"></image>
+				</swiper-item>
+
+			</swiper>
+		</view>
+		<view class="cartList">
+
+			<view class="cartItem" v-for="(item,index) in cartlist">
+
+				<image :src="item.img" mode="aspectFit"></image>
+				{{item.title}}
+			</view>
+		</view>
+		<view class="main">
+			主体区域
+		</view>
+	</view>
+
 </template>
 
 <script setup>
@@ -15,6 +36,16 @@
 	} from "vue";
 
 	const token = ref("")
+	const cartlist = ref([{
+		title: "最新文章",
+		img: "../../static/tabbar/1.png"
+	}, {
+		title: "相册",
+		img: "../../static/tabbar/2.png"
+	}, {
+		title: "网课",
+		img: "../../static/tabbar/3.png"
+	}])
 	onReady(x => {
 		token.value = localStorage.getItem("token");
 		// console.log("read函数", token.value)
@@ -39,5 +70,57 @@
 </script>
 
 <style lang="scss" scoped>
+	.body{
+		background-color: #f3f3f3;
+		height: 100vh;
+	}
+	
+	.swpierBox {
+		// background-color: red;
+		width: 750rpx;
+		height: 320rpx;
 
+		swiper {
+			background-color: red;
+			height: 100%;
+			width: 100%;
+
+			swiper-item {
+				height: 100%;
+				width: 100%;
+
+				image {
+					height: 100%;
+					width: 100%;
+				}
+
+			}
+		}
+	}
+
+	.cartList {
+
+		margin: 15rpx;
+
+		height: 150rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+
+		.cartItem {
+			width: 150rpx;
+			height: 100%;
+			// background-color: red;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			image {
+				height: 80%;
+				width: 100%;
+			}
+		}
+		.cartItem:hover{
+			background-color: wheat;
+		}
+	}
 </style>
